2a0e8ff33f3702d7ecfaece6db99645ef2d75324
Paulo Arthur <pauloarthur.ce@gmail.com>
2023-11-23 17:35:22 -0300

    testando branch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //teste main
         //fiz uma alteração
+        //testando brench
         System.out.printf("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
 
